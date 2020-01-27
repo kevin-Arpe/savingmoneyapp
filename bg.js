cb1945397acd38dd1a626d34eb5a3a1eaa7fa765
kevin-Arpe <57397003+kevin-Arpe@users.noreply.github.com>
2020-01-27 15:32:06 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,21 +1,14 @@
 let body= document.querySelector("body");
-let IMG_NUMBER = 5;
 
-function getRandom() {
-    let num = Math.floor(Math.random() * IMG_NUMBER);
-    return num;
-}
-
-function paintImage(num) {
+function paintImage() {
     let image = new Image();
-    image.src = `${num + 1}.jpg`;
+    image.src = `1.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
 
 function init() {
-    let randomNum = getRandom();
-    paintImage(randomNum);
+    paintImage();
 }
 
 init();
--- calculator.js
+++ calculator.js
@@ -64,8 +64,8 @@ let DISPLAY_CN = "display";
 let goal_LS = "goal";
 
 
-function saveItem(name, num) {
-    localStorage.setItem(name, num)
+function saveItem(name, item) {
+    localStorage.setItem(name, item)
 }
 
 function paintValue() {
@@ -92,9 +92,108 @@ function paintValue() {
 function paintGoal(num) {
     goal_input.classList.add(DISPLAY_CN);
     goal_span.classList.remove(DISPLAY_CN);
-    goal_span.innerHTML = num +"원";
+    goal_span.innerHTML = num +"만원";
 }
 // function paintGoal
+
+let DEPOSIT_LS = "deposit";
+let dep = [];
+
+function getDeposit() {
+    let current_deposit = JSON.parse(localStorage.getItem(DEPOSIT_LS));
+    dep = current_deposit;
+
+    if (current_deposit !== null) {
+        paintDeposit();
+    } else {
+
+    }
+}
+
+function paintDeposit() {
+    let first = document.querySelector(".first"),
+        first_jan = first.querySelector(".jan"),
+        first_feb = first.querySelector(".feb"),
+        first_mar = first.querySelector(".mar"),
+        first_apr = first.querySelector(".apr"),
+        first_may = first.querySelector(".may"),
+        first_jun = first.querySelector(".jun"),
+        first_jul = first.querySelector(".jul"),
+        first_aug = first.querySelector(".aug"),
+        first_sep = first.querySelector(".sep"),
+        first_oct = first.querySelector(".oct"),
+        first_nov = first.querySelector(".nov"),
+        first_dec = first.querySelector(".dec");
+
+    let second = document.querySelector(".second"),
+        second_jan = second.querySelector(".jan"),
+        second_feb = second.querySelector(".feb"),
+        second_mar = second.querySelector(".mar"),
+        second_apr = second.querySelector(".apr"),
+        second_may = second.querySelector(".may"),
+        second_jun = second.querySelector(".jun"),
+        second_jul = second.querySelector(".jul"),
+        second_aug = second.querySelector(".aug"),
+        second_sep = second.querySelector(".sep"),
+        second_oct = second.querySelector(".oct"),
+        second_nov = second.querySelector(".nov"),
+        second_dec = second.querySelector(".dec");
+
+    let last = document.querySelector(".last"),
+        last_jan = last.querySelector(".jan"),
+        last_feb = last.querySelector(".feb"),
+        last_mar = last.querySelector(".mar"),
+        last_apr = last.querySelector(".apr"),
+        last_may = last.querySelector(".may"),
+        last_jun = last.querySelector(".jun"),
+        last_jul = last.querySelector(".jul"),
+        last_aug = last.querySelector(".aug"),
+        last_sep = last.querySelector(".sep"),
+        last_oct = last.querySelector(".oct"),
+        last_nov = last.querySelector(".nov"),
+        last_dec = last.querySelector(".dec");
+
+    let month_array = [
+        first_jan, first_feb, first_mar, first_apr, first_may, first_jun, first_jul, first_aug, first_sep, first_oct, first_nov, first_dec,
+        second_jan, second_feb, second_mar, second_apr, second_may, second_jun, second_jul, second_aug, second_sep, second_oct, second_nov, second_dec,
+        last_jan, last_feb, last_mar, last_apr, last_may, last_jun, last_jul, last_aug, last_sep, last_oct, last_nov, last_dec
+    ]
+
+    for (i = 0; i < dep.length; i++) {
+        month_array[i].innerHTML = dep[i];
+    }
+
+}
+
+function addDeposit() {
+    let ave_form = document.querySelector(".js-ave_form"),
+        ave_txt = ave_form.querySelector(".txt"),
+        ave_btn = ave_form.querySelector(".btn");
+
+    ave_form.addEventListener("submit", function(event) {
+        let currentValue = ave_txt.value;
+
+        if (isNaN(currentValue)) {
+            alert("금액은 숫자만 입력가능합니다.")
+            return null;
+        } else {
+            dep.push(currentValue);
+            ave_txt.value = "";
+            saveItem(DEPOSIT_LS, JSON.stringify(dep));
+        }
+    })
+
+    ave_btn.addEventListener("click", function(event) {
+        let current_deposit = JSON.parse(localStorage.getItem(DEPOSIT_LS));
+        dep = current_deposit;
+        dep.pop();
+        saveItem(DEPOSIT_LS, JSON.stringify(dep));
+        location.reload(true);
+        })
+}
+
+
+// monthly data
 
 function init() {
     getTime();
@@ -104,6 +203,11 @@ function init() {
 
     paintPassedMonth();
     paintRemainMonth();
+
+    getDeposit();
+    addDeposit();
+
+    
 }
 
 init();
